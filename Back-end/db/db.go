ed6eac25303fb958bd8590ff7a029c9336a659e2
João Vitor Espig <jotinha1300@gmail.com>
2022-12-03 17:01:05 -0300

    transformado para função anônima

--- Back-end/db/db.go
+++ Back-end/db/db.go
@@ -5,12 +5,10 @@ import (
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB = openDB()
-
-func openDB() *gorm.DB {
+var DB *gorm.DB = func() *gorm.DB {
 	newDB, err := gorm.Open(sqlite.Open("./db/db.db"))
 	if err != nil {
 		panic(err)
 	}
 	return newDB
-}
+}()
