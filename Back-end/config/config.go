e291a91b131e9843fe35e5926a9f100dd9b1914f
João Vitor Espig <jotinha1300@gmail.com>
2022-12-03 18:11:08 -0300

    adicionado redirecionamento

--- Back-end/config/config.go
+++ Back-end/config/config.go
@@ -11,6 +11,9 @@ func CreateEngine() *gin.Engine {
 
 	engine.Static("/frontend/", "./../Front-end/")
 
+	engine.GET("/", func(ctx *gin.Context) {
+		ctx.Redirect(301, "/frontend/escolhas.html")
+	})
 	engine.GET("/answer/get", controllers.GetAnswersByName())
 	engine.GET("/answer/get_all", controllers.GetAnswers())
 	engine.PUT("/answer/increase_streak", controllers.IncreaseStreak())
